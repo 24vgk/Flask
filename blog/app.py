0801601d6569bb0ab792af885c0fb17d7e93b924
andrejkalabin <admin@24vgk.ru>
2023-04-06 15:25:12 +0300

    lesson9 complete

--- blog/app.py
+++ blog/app.py
@@ -11,6 +11,7 @@ from flask_migrate import Migrate
 from blog.security import flask_bcrypt
 from blog.views.authors import authors_app
 from blog.admin import admin
+# from blog.api import init_api
 
 app = Flask(__name__)
 app.register_blueprint(users_app, url_prefix="/users")
@@ -31,6 +32,7 @@ app.config.from_object(f"blog.config.{cfg_name}")
 migrate = Migrate(app, db, compare_type=True)
 flask_bcrypt.init_app(app)
 admin.init_app(app)
+# api = init_api(app)
 
 
 @app.route("/")
